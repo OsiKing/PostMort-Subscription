2671bac05984a60a719d2b15a7233986fe64030e
Callistus Enekhaze <44670996+OsiKing@users.noreply.github.com>
2020-07-13 23:27:04 +0100

    Update header.php
    Updated links

--- templates/header.php
+++ templates/header.php
@@ -20,7 +20,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top header-bg-dark" style="background: ##FFFFFF!;">
   <div class="container">
-    <a class="navbar-brand font-weight-bold" href="#"><h1>PostMort</h1></a>
+    <a class="navbar-brand font-weight-bold" href="https://postmort-subscription.herokuapp.com/"><h1>PostMort</h1></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
